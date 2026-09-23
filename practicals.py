81f4bfe202d14fbc96a0ebf72401b96fe2aed1e2
Nurmuhammad Mirzaahmadov <runnp3ddd@gmail.com>
2026-09-22 20:02:52 -0400

    Codes

--- practicals.py
+++ practicals.py
@@ -55,8 +55,24 @@
 #print("And your no traffic commute time is")
 #print(no_traffic_commute_time)
 
-seconds = 95399
-minutes = seconds // 60
-hours = minutes // 60
-days = hours // 24
-print(seconds, minutes, hours, days)
+#seconds = 95399
+#minutes = seconds // 60
+#hours = minutes // 60
+#days = hours // 24
+#print(seconds, minutes, hours, days)
+
+def leetcodes_answered(num_weeks: int, questions_per_week: int) -> int:
+    '''Return the total number of leetcode questions done over num_weeks
+    where in each week questions_per_week were answered
+    >>> leetcodes_answered(2, 30)
+    60
+    >>> leetcodes_answered(1, 8)
+    8
+    '''
+    return num_weeks * questions_per_week
+# two of us are doing leetcode but at different rates and different amounts of time
+person_a = leetcodes_answered(2, 8)
+person_b_rate = 30
+person_b = leetcodes_answered(1, person_b_rate)
+grand_total = person_a + person_b
+print("With the two of us combined, we will have done", grand_total, "questions")
